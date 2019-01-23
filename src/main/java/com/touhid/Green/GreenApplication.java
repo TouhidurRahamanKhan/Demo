2ac95e1825d585c8b87312fb6d52552e17cad041
touhid <touhid@therapservices.net>
2019-01-23 09:51:09 +0600

    App initilize

---
+++ src/main/java/com/touhid/Green/GreenApplication.java
@@ -0,0 +1,23 @@
+package com.touhid.Green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenApplication.class, args);
+	}
+
+@RequestMapping("/Hello")
+	public String hello()
+	{
+		return "Hello";
+	}
+
+}
+
